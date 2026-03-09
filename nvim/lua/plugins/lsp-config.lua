694dc38820baa35ddbdb7e0c1a5a99f8ad665d50
ilkerciblak <ilkerciblak@gmail.com>
2026-03-09 09:30:36 +0300

    build(dev-env): add tsx and html emmets to development environment

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,62 +1,98 @@
----@diagnostic disable: undefined-global
 return {
-	{
-		"mason-org/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"mason-org/mason-lspconfig.nvim",
-		opts = {},
-		dependencies = {
-			{ "mason-org/mason.nvim", opts = {} },
-			"neovim/nvim-lspconfig",
-		},
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"gopls",
-          "tsserver",
-				},
-				automatic_installation = true,
-			})
-		end,
-	},
-	-- NVIM LSP CONFIG PLUGIN
+  {
+    "mason-org/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
 
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      "mason-org/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "gopls",
+          "ts_ls",
+          "html",
+          "emmet_ls",
+        },
+        automatic_installation = true,
+      })
+    end,
+  },
 
-			-- An example for configuring `clangd` LSP to use nvim-cmp as a completion engine
-			vim.lsp.config("lua_ls", {
-				capabilities = capabilities,
-			})
-			vim.lsp.enable("lua_ls")
-			vim.lsp.config("gopls", {
-				cmd = { "gopls" },
-				filetypes = { "go", "gomod", "gowork", "gotmpl" },
-				capabilities = capabilities,
-				settings = {
-					analyses = {
-						unusedparams = true,
-						shadow = true,
-					},
-          usePlaceholders = true,
-					staticcheck = true,
-				},
-			})
-			vim.lsp.enable("gopls")
-			vim.lsp.enable("gofmt")
-      vim.lsp.enable("tsserver")
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("i", "<C-K>", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      -- LUA
+      vim.lsp.config("lua_ls", {
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+          },
+        },
+      })
+
+
+      -- GO
+      vim.lsp.config("gopls", {
+        capabilities = capabilities,
+        settings = {
+          gopls = {
+            analyses = {
+              unusedparams = true,
+              shadow = true,
+            },
+            staticcheck = true,
+            usePlaceholders = true,
+          },
+        },
+      })
+
+      -- TYPESCRIPT / TSX
+      vim.lsp.config("ts_ls", {
+        capabilities = capabilities,
+      })
+
+      -- HTML
+      vim.lsp.config("html", {
+        capabilities = capabilities,
+      })
+
+      -- EMMET (HTML snippet for TSX)
+      vim.lsp.config("emmet_ls", {
+        capabilities = capabilities,
+        filetypes = {
+          "html",
+          "css",
+          "scss",
+          "javascriptreact",
+          "typescriptreact",
+        },
+      })
+
+      vim.lsp.enable("lua_ls")
+      vim.lsp.enable("gopls")
+      vim.lsp.enable("ts_ls")
+      vim.lsp.enable("html")
+      vim.lsp.enable("emmet_ls")
+
+
+      -- Keymaps
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
